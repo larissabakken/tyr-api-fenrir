b85b3799c97600797e2b2eb9c9972258356cbca8
Larissa Ramos <devlarissasr@gmail.com>
2023-02-23 18:56:43 -0300

    feat(auth: created files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
-  controllers: [AppController],
+  imports: [PrismaModule, UserModule, AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
