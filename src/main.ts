7b7a06e746b261654d318ecdbc71787c45e52dc2
Larissa Ramos <devlarissasr@gmail.com>
2023-02-23 19:49:47 -0300

    feat: interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedInterceptor } from './interceptors/Unauthorized.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Interceptors
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
 
   await app.listen(3030, () => {
     console.log('Server is running on port 3030');
