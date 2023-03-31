9b14f168c293a242466ca0d9bc630755af83010d
Larissa Ramos <devlarissasr@gmail.com>
2023-03-31 08:52:09 -0300

    :construction: dtos driver

--- src/drivers/dto/create-driver.dto.ts
+++ src/drivers/dto/create-driver.dto.ts
@@ -1,1 +1,36 @@
-export class CreateDriverDto {}
+import { IsString, IsNotEmpty, IsBoolean, IsOptional, Matches, ValidateIf } from 'class-validator';
+import { Driver } from '../entities/driver.entity';
+
+export class CreateDriverDto implements Driver{
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @ValidateIf((obj) => !obj.cpf || !obj.cnpj, { message: 'CPF or CNPJ is required' } )
+    @Matches(/^[0-9]{11}$/, { message: 'Invalid CPF' })
+    cpf: string;
+
+    @IsString()
+    @ValidateIf((obj) => !obj.cpf || !obj.cnpj, { message: 'CPF or CNPJ is required' } )
+    @Matches(/^[0-9]{14}$/, { message: 'Invalid CNPJ' })
+    cnpj: string;
+
+
+    @IsString()
+    @IsOptional()
+    @Matches(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/, { message: 'Invalid email' })
+    email: string;
+
+    @IsString()
+    @Matches(/^[0-9]{10,11}$/, { message: 'Invalid phone number' })
+    phone: string;
+
+    @IsOptional()
+    @IsString()
+    address?: string;
+
+    @IsBoolean()
+    @ValidateIf((obj) => obj.status === undefined || obj.status === null? obj.status = false : obj.status)
+    status: boolean;
+}
--- src/drivers/dto/update-driver.dto.ts
+++ src/drivers/dto/update-driver.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDriverDto } from './create-driver.dto';
+import { IsString, IsNotEmpty } from 'class-validator';
 
-export class UpdateDriverDto extends PartialType(CreateDriverDto) {}
+export class UpdateDriverDto extends PartialType(CreateDriverDto) {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+}
