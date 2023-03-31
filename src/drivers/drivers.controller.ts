b65009cc1ec094aec65bc3c8a652f53744e78626
Larissa Ramos <devlarissasr@gmail.com>
2023-03-31 09:10:31 -0300

    :construction: controllers

--- src/drivers/drivers.controller.ts
+++ src/drivers/drivers.controller.ts
@@ -1,34 +1,65 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { DriversService } from './drivers.service';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
+import { Public } from 'src/auth/public.decorator';
 
+@Public() // This decorator is used to allow access to this controller without authentication
 @Controller('drivers')
 export class DriversController {
   constructor(private readonly driversService: DriversService) {}
 
-  @Post()
+  @Post('/create')
   create(@Body() createDriverDto: CreateDriverDto) {
     return this.driversService.create(createDriverDto);
   }
 
   @Get()
-  findAll() {
-    return this.driversService.findAll();
+  async findAll(
+    @Query('page') page: string,
+    @Query('limit') limit: string,
+  ): Promise<{ data: any[]; total: number }> {
+    const drivers = await this.driversService.findAll(+page, +limit);
+    return drivers;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.driversService.findOne(+id);
+    return this.driversService.findById(id);
+  }
+
+  @Get('')
+  async findAllByValue(
+    @Query('cpf') cpf: string,
+    @Query('cnpj') cnpj: string,
+    @Query('email') email: string,
+    @Query('name') name: string,
+  ) {
+    const drivers = await this.driversService.findAllByValue(
+      cpf,
+      cnpj,
+      email,
+      name,
+    );
+    return drivers;
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDriverDto: UpdateDriverDto) {
-    return this.driversService.update(+id, updateDriverDto);
+    return this.driversService.update(id, updateDriverDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.driversService.remove(+id);
+    return this.driversService.remove(id);
   }
 }
--- src/drivers/drivers.service.ts
+++ src/drivers/drivers.service.ts
@@ -1,26 +1,77 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDriverDto } from './dto/create-driver.dto';
 import { UpdateDriverDto } from './dto/update-driver.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class DriversService {
-  create(createDriverDto: CreateDriverDto) {
-    return 'This action adds a new driver';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createDriverDto: CreateDriverDto) {
+    const status = createDriverDto.status;
+
+    if (status !== true && status !== false && status !== null) {
+      createDriverDto.status = false;
+    }
+
+    const isCpfCnpjExists = await this.prisma.driver.findUnique({
+      where: {
+        cpf: createDriverDto.cpf || createDriverDto.cnpj,
+      },
+    });
+
+    if (isCpfCnpjExists) {
+      throw new Error('CPF/CNPJ already exists');
+    }
+
+    const data: Prisma.DriverCreateInput = {
+      ...createDriverDto,
+    };
+
+    const createdDriver = await this.prisma.driver.create({ data });
+
+    return {
+      ...createdDriver,
+    };
   }
 
-  findAll() {
-    return `This action returns all drivers`;
+  async findAll(
+    page: number,
+    limit: number,
+  ): Promise<{ data: any[]; total: number }> {
+    const skip = (page - 1) * limit;
+    const take = limit;
+    const customers = await this.prisma.driver.findMany({
+      skip: isNaN(skip) ? 0 : skip,
+      take: isNaN(take) ? 2 : take,
+    });
+    const total = await this.prisma.driver.count();
+    return { data: customers, total };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} driver`;
+  async findById(id: string) {
+    if (!id) {
+      throw new Error('ID is required');
+    }
+    return await this.prisma.driver.findUnique({ where: { id } });
   }
 
-  update(id: number, updateDriverDto: UpdateDriverDto) {
-    return `This action updates a #${id} driver`;
+  async findAllByValue( cpf:string, cnpj:string, email:string, name:string) {
+    if (!cpf && !cnpj && !email && !name) {
+      throw new Error('CPF, CNPJ, EMAIL or NAME is required');
+    }
+    return await this.prisma.driver.findMany({ where: { cpf, cnpj, email, name } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} driver`;
+  async update(id: string, updateDriverDto: UpdateDriverDto) {
+    return await this.prisma.driver.update({
+      where: { id },
+      data: updateDriverDto,
+    });
+  }
+
+  async remove(id: string) {
+    return await this.prisma.driver.delete({ where: { id } });
   }
 }
