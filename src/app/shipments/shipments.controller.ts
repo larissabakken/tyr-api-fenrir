d734605981be8e85ff1e40af96b80234125380d3
Larissa Ramos <devlarissasr@gmail.com>
2023-04-16 15:44:01 -0300

    :doc: shipments

--- src/app/shipments/shipments.controller.ts
+++ src/app/shipments/shipments.controller.ts
@@ -6,44 +6,129 @@ import {
   Patch,
   Param,
   Delete,
-  Put,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiTags,
+  ApiQuery,
+  ApiParam,
+} from '@nestjs/swagger';
 import { ShipmentsService } from './shipments.service';
 import { CreateShipmentDto } from './dto/create-shipment.dto';
 import { UpdateShipmentDto } from './dto/update-shipment.dto';
 import { Public } from 'src/auth/public.decorator';
+import { Shipment } from './entities/shipment.entity';
 
 @Public()
+@ApiTags('shipments')
 @Controller('shipments')
 export class ShipmentsController {
   constructor(private readonly shipmentsService: ShipmentsService) {}
 
   @Post('create')
+  @ApiOperation({ summary: 'Create shipment' })
+  @ApiBody({
+    type: Shipment,
+    examples: { 'application/json': {} },
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The created record',
+    type: Shipment,
+  })
   create(@Body() createShipmentDto: CreateShipmentDto) {
     return this.shipmentsService.create(createShipmentDto);
   }
 
   @Get(':id/vehicle/:vehicleId')
+  @ApiOperation({ summary: 'Add vehicle to shipment' })
+  @ApiParam({
+    name: 'id',
+    description: "Shipment's Id",
+    required: true,
+    type: String,
+  })
+  @ApiParam({
+    name: 'vehicleId',
+    description: "Vehicle's Id",
+    required: true,
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The updated record',
+  })
   addVehicle(@Param('id') id: string, @Param('vehicleId') vehicleId: string) {
     return this.shipmentsService.addVehicle(id, vehicleId);
   }
 
   @Get(':id/cart/:cartId')
+  @ApiOperation({ summary: 'Add cart to shipment' })
+  @ApiParam({
+    name: 'id',
+    description: "Shipment's Id",
+    required: true,
+    type: String,
+  })
+  @ApiParam({
+    name: 'cartId',
+    description: "Cart's Id",
+    required: true,
+    type: String,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The updated record',
+  })
   addCart(@Param('id') id: string, @Param('idCart') cartId: string) {
     return this.shipmentsService.addCart(id, cartId);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Find all shipments' })
+  @ApiResponse({
+    status: 200,
+    description: 'The found records',
+    type: Shipment,
+  })
   findAll() {
     return this.shipmentsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Find one shipment' })
+  @ApiParam({
+    name: 'id',
+    description: "Shipment's Id",
+    required: true,
+    type: String,
+  })
   findOne(@Param('id') id: string) {
     return this.shipmentsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update shipment' })
+  @ApiParam({
+    name: 'id',
+    description: "Shipment's Id",
+    required: true,
+    type: String,
+  })
+  @ApiBody({
+    type: Shipment,
+    examples: { 'application/json': {} },
+    required: true,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The updated record',
+    type: Shipment,
+  })
   update(
     @Param('id') id: string,
     @Body() updateShipmentDto: UpdateShipmentDto,
@@ -65,5 +150,4 @@ export class ShipmentsController {
   removeCart(@Param('id') id: string) {
     return this.shipmentsService.removeCart(id);
   }
-  
 }
