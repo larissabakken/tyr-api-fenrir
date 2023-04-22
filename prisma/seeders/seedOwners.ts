9fe97cb156a663220a10740c2aa421cd31b8ef99
Larissa Ramos <devlarissasr@gmail.com>
2023-04-22 17:00:04 -0300

    :rocket:

--- prisma/seeders/seedOwners.ts
+++ prisma/seeders/seedOwners.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import * as casual from 'casual';
 
 export async function seedOwners(prisma: PrismaClient) {
-  const owners = new Array(50).fill(null).map(() => ({
+  const owners = new Array(10).fill(null).map(() => ({
     name: casual.name,
     cpf: casual.numerify('###########'),
     cnpj: casual.numerify('##############'),
